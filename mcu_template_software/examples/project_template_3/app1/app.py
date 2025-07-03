26dffa3942da3a7c0efb36e4cbb84dbfb1d73f04
Paul Austen <pjaos@gmail.com>
2025-07-03 14:18:54 +0100

    Update the example_3 and example_4 mcu code to add webserver that can be used to serve dynamic files.

--- mcu_template_software/examples/project_template_3/app1/app.py
+++ mcu_template_software/examples/project_template_3/app1/app.py
@@ -1,6 +1,7 @@
 # from machine import WDT
 
 import os
+import asyncio
 from time import time, sleep
 
 from lib.uo import UO, UOBase
@@ -131,6 +132,25 @@ class ThisMachine(BaseMachine):
         # The WDT will then trigger a reboot.
     #        self._wdt = WDT(timeout=WDT_TIMEOUT_MSECS)
 
+    async def _updateTemp(self, paramDict):
+        """@brief Example code to update the temperature and humidity.
+                  In reality you are likely to read values from sensor/s. """
+        from math import floor
+        from random import random
+        #This task runs while the webserver is running.
+        while True:
+            # Move temp and humidity by small amounts so user can see
+            # values changing
+            temp = paramDict['temperature']
+            value = floor(float(temp)) + random()
+            paramDict['temperature'] = f"{value:.3f}"
+
+            humidity = paramDict['humidity']
+            value = floor(float(humidity)) + random()
+            paramDict['humidity'] = f"{value:.3f}"
+
+            await asyncio.sleep(1)
+
     def start(self):
         self.show_ram_info()
 
@@ -141,4 +161,7 @@ class ThisMachine(BaseMachine):
         web_server = WebServer(self._machine_config,
                                self._startTime,
                                uo=self._uo)
+        paramDict = {'temperature': 24.5, 'humidity': 60}
+        asyncio.create_task(self._updateTemp(paramDict))
+        web_server.setParamDict(paramDict)
         web_server.run()
