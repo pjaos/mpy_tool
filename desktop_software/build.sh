7de7355329b3dd21e42d00a943eb7110ba41afd9
Paul Austen <pjaos@gmail.com>
2025-07-16 09:29:24 +0100

    Update so we end up with the build output in the linux folder.

--- desktop_software/build.sh
+++ desktop_software/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-set -e
-rm -rf dist
-rm -rf linux
+rm dist/*.tar.gz
+rm dist/*.whl
 ./check_code.sh
 # Use poetry command to build python wheel
 poetry -vvv build
-mv dist linux
+rm linux/*.tar.gz 2>&1 > /dev/null
+rm linux/*.whl 2>&1 > /dev/null
+set -e
+cp dist/*.tar.gz linux
+cp dist/*.whl linux
+rm -rf dist
