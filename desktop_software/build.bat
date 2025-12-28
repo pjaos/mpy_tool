248c171f2b21b3a8a8ef86e88603f6db77c2c9ee
Paul Austen <pausten.os@gmail.com>
2025-12-28 22:33:32 +0000

    Add the examples to the windows python wheel for windows.

--- desktop_software/build.bat
+++ desktop_software/build.bat
@@ -1,5 +1,7 @@
 rmdir /s /q dist
 rmdir /s /q windows
+python copy_examples.py # Copy the examples folder to the assets folder so
+                         # that the examples are available once installed.
 # Use poetry command to build python wheel
 poetry -vvv build
 move dist windows
