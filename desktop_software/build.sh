1edb10623c29f1a2473fd6e407cc606e44962590
Paul Austen <pjaos@gmail.com>
2026-01-12 06:57:54 +0000

    output directly to the linux folder and remove the tar.gz file

--- desktop_software/build.sh
+++ desktop_software/build.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-rm dist/*.tar.gz
-rm dist/*.whl
 ./check_code.sh # Run some checks on the code before building it
 cp pyproject.toml src/mpy_tool/assets
 python3 copy_examples.py # Copy the examples folder to the assets folder so
                          # that the examples are available once installed.
 # Use poetry command to build python wheel
-poetry -vvv build
-rm linux/*.tar.gz 2>&1 > /dev/null
-rm linux/*.whl 2>&1 > /dev/null
-#set -e
-cp dist/*.tar.gz linux
-cp dist/*.whl linux
-rm -rf dist
+poetry --output=linux --clean -vvv build
+# Delete the .tar.gz file in dist directory
+rm -f linux/*.tar.gz
