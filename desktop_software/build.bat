5d499d95bd0645301ce0be150e42bdb0d00a6999
Paul Austen <pausten.os@gmail.com>
2026-01-12 15:20:21 +0000

    Update to build the python wheel on windows

--- desktop_software/build.bat
+++ desktop_software/build.bat
@@ -1,8 +1,10 @@
 copy /Y "pyproject.toml" "src/mpy_tool/assets"
-rmdir /s /q dist
-rmdir /s /q windows
-python copy_examples.py # Copy the examples folder to the assets folder so
-                         # that the examples are available once installed.
-# Use poetry command to build python wheel
-poetry -vvv build
-move dist windows
+:: Copy the examples folder to the assets folder so that the examples are available once installed.
+python copy_examples.py 
+:: Use poetry command to build python wheel
+poetry --output=windows --clean -vvv build 
+:: We want windows in the filename so that both the linux/macos wheels and the windows wheels can be added to github releases
+:: Delete the .tar.gz file in windows directory (if it exists)
+del "windows\*.tar.gz"
+:: Put a copy of the install.py alongside the python wheel
+copy .\install.py .\windows\
